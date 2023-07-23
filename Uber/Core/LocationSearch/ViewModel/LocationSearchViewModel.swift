3ea01093805307b72920250c483117a65efc6732
Ricardo <100056112+ricardotech@users.noreply.github.com>
2023-07-23 10:08:15 -0300

    Getting Location Coordinates

--- Uber/Core/LocationSearch/ViewModel/LocationSearchViewModel.swift
+++ Uber/Core/LocationSearch/ViewModel/LocationSearchViewModel.swift
@@ -13,6 +13,8 @@ class LocationSearchViewModel: NSObject, ObservableObject {
     // MARK: - Properties
     
     @Published var results = [MKLocalSearchCompletion]()
+    @Published var selectedLocationCoordinate: CLLocationCoordinate2D?
+    
     private let searchCompleter = MKLocalSearchCompleter()
     var queryFragment: String = "" {
         didSet {
@@ -24,6 +26,27 @@ class LocationSearchViewModel: NSObject, ObservableObject {
         super.init()
         searchCompleter.delegate = self
         searchCompleter.queryFragment = queryFragment
+    }
+    
+    func selectLocation(_ localSearch: MKLocalSearchCompletion) {
+        locationSearch(forLocalSearchCompletion: localSearch) {
+            response, error in
+            if let error = error {
+                print("DEBUG: Location search failed with error \(error.localizedDescription)")
+            }
+            guard let item = response?.mapItems.first else { return }
+            let coordinate = item.placemark.coordinate
+            self.selectedLocationCoordinate = coordinate
+            print("DEBUG: Location coordinate \(coordinate)")
+        }
+    }
+    
+    func locationSearch(forLocalSearchCompletion localSearch: MKLocalSearchCompletion,              completion: @escaping MKLocalSearch.CompletionHandler) {
+            let searchRequest = MKLocalSearch.Request()
+            searchRequest.naturalLanguageQuery = localSearch.title.appending(localSearch.subtitle)
+            let search = MKLocalSearch(request: searchRequest)
+        
+            search.start(completionHandler: completion)
     }
 }
 
--- Uber/Core/LocationSearch/Views/LocationSearchView.swift
+++ Uber/Core/LocationSearch/Views/LocationSearchView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct LocationSearchView: View {
     @State private var startLocationText = ""
-    @State private var destinationLocationText = ""
-    @StateObject var viewModel = LocationSearchViewModel()
+    @Binding var showLocationSearchView: Bool
+    @EnvironmentObject var viewModel: LocationSearchViewModel
     
     var body: some View {
         VStack {
@@ -54,6 +54,11 @@ struct LocationSearchView: View {
                 VStack(alignment: .leading) {
                     ForEach(viewModel.results, id: \.self) { result in
                         LocationSearchResultsCell(title: result.title, subtitle: result.subtitle)
+                            .onTapGesture {
+                                viewModel.selectLocation(result)
+                                showLocationSearchView.toggle()
+                            
+                        }
                     }
                 }
             }
@@ -64,6 +69,6 @@ struct LocationSearchView: View {
 
 struct LocationSearchView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationSearchView()
+        LocationSearchView(showLocationSearchView: .constant(false))
     }
 }
